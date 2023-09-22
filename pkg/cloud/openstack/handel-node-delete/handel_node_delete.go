d2aeeae7d19bcb1c13b114b7df4f233513ae2201
WassimDhib <30473593+WassimDhib@users.noreply.github.com>
2023-09-22 16:42:47 +0200

    Update handel_node_delete.go
    gophercloud

--- pkg/cloud/openstack/handel-node-delete/handel_node_delete.go
+++ pkg/cloud/openstack/handel-node-delete/handel_node_delete.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"github.com/Chathuru/kubernetes-cluster-autoscaler/pkg/cloud/openstack"
 	"github.com/Chathuru/kubernetes-cluster-autoscaler/pkg/common/datastructures"
-	"github.com/rackspace/gophercloud/openstack/compute/v2/servers"
+	"github.com/rackspace/gophercloud/gophercloud/compute/v2/servers"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
